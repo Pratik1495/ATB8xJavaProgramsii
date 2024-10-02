735fd9e332966e508c6db8f8082119030c200732
Asus <pratik.bhopal.patil92@gmail.com>
2024-10-02 21:17:19 +0530

    Task of thew day

---
+++ src/Task/Q.java
@@ -0,0 +1,15 @@
+package Task;
+
+import java.util.SortedSet;
+
+public class Q {
+
+
+    public static void main(String[] args) {
+
+        int a= 10;
+        System.out.println(a-- +a-- +a--);
+        System.out.println(a);
+
+    }
+}
