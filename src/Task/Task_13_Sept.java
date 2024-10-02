eeb5f5e87c8da5c02b6ff62bb922e0f1e7af76a3
Asus <pratik.bhopal.patil92@gmail.com>
2024-10-02 23:30:14 +0530

    Task of thew day

---
+++ src/Task/Task_13_Sept.java
@@ -0,0 +1,20 @@
+package Task;
+
+public class Task_13_Sept {
+    public static void main(String[] args) {
+        int num=10;
+        System.out.printf("%d * %d =%d" ,num,1,num*1);
+        System.out.println();
+        System.out.printf("%d * %d =%d",num,2,num*2);
+        System.out.println();
+        System.out.printf("%d * %d =%d",num,3,num*3);
+
+        double a = 34;
+        double b = 10;
+        System.out.println("Addition :"+(a+b));
+        System.out.println("Subtraction :"+ (a-b));
+        System.out.println("Division:"+a/b);
+        System.out.println("Multiplication :"+a*b);
+        System.out.println("Modulas :"+a%b);
+    }
+}
