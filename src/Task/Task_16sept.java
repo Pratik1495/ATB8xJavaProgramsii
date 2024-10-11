3d6ca6f5240db7efc74300f60f0669bb3bccff84
Asus <pratik.bhopal.patil92@gmail.com>
2024-10-11 12:52:26 +0530

    Task_20_Sept

---
+++ src/Task/Task_16sept.java
@@ -0,0 +1,14 @@
+package Task;
+
+public class Task_16sept {
+    public static void main(String[] args) {
+        byte b=10;
+        byte c=20;
+        int a= b+c;
+
+
+
+
+        System.out.println(b+c);
+    }
+}
